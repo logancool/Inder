e7cb44ff379a5c3aafa032dcd699eadec99df554
loganflashtalking <logan.cool@flashtalking.com>
2016-01-21 15:55:28 -0800

    simple footer refactoring

--- client/components/AppBody.jsx
+++ client/components/AppBody.jsx
@@ -5,17 +5,46 @@ AppBody = React.createClass({
     getMeteorData() {
         return {}
     },
-
+    getDefaultProps() {
+        return {
+            tabs: ["Tab 1", "Tab 2", "Tab 3"]
+        }
+    },
+    getInitialState() {
+        return {
+            modal: false
+        }
+    },
+    getAppShell() {
+        <Home />
+    },
+    ionModal(tab) {
+        this.setState({
+            modal: (
+                <IonModal>
+                    <div className="h1 title">{tab}</div>
+                    <button onClick={ () => this.setState({modal:false}) } className="button button-icon active">
+                        <i className="icon ion-ios-close-empty"></i>
+                    </button>
+                </IonModal>
+            )
+        })
+    },
     render() {
         return (
-            <div className="ionic-body">
-                <div className="view">
-                    <div className="scroll-content ionic-scroll">
-                        <div className="content overflow-scroll has-header has-footer">
-                            {this.props.children}
-                        </div>
-                    </div>
-                </div>
+            <div className="tabs tabs-icon-top">
+                {this.state.modal ? <Backdrop /> : false}
+                <Transition transitionName="modal" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+                    {this.state.modal}
+                </Transition>
+                {this.props.tabs.map((tab, i) => {
+                    return (
+                        <a className="tab-item" key={tab} onClick={this.ionModal.bind(null, tab)}>
+                            <i className="icon ion-star"></i>
+                            {tab}
+                        </a>
+                    )
+                })}
             </div>
         )
     }
--- client/layout/IonFooter.jsx
+++ client/layout/IonFooter.jsx
@@ -5,47 +5,9 @@ IonFooter = React.createClass({
     getMeteorData() {
         return {}
     },
-    getDefaultProps() {
-        return {
-            tabs: ["Tab 1", "Tab 2", "Tab 3"]
-        }
-    },
-    getInitialState() {
-        return {
-            modal: false
-        }
-    },
-    getAppShell() {
-        <Home />
-    },
-    ionModal(tab) {
-        this.setState({
-            modal: (
-                <IonModal>
-                    <div className="h1 title">{tab}</div>
-                    <button onClick={ () => this.setState({modal:false}) } className="button button-icon active">
-                        <i className="icon ion-ios-close-empty"></i>
-                    </button>
-                </IonModal>
-            )
-        })
-    },
     render() {
         return (
-            <div className="tabs tabs-icon-top">
-                {this.state.modal ? <Backdrop /> : false}
-                <Transition transitionName="modal" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
-                    {this.state.modal}
-                </Transition>
-                {this.props.tabs.map((tab, i) => {
-                    return (
-                        <a className="tab-item" key={tab} onClick={this.ionModal.bind(null, tab)}>
-                            <i className="icon ion-star"></i>
-                            {tab}
-                        </a>
-                    )
-                })}
-            </div>
+            <div className="nothing"></div>
         )
     }
 });
